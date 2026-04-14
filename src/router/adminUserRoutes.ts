d85ac21d37528c3e1c93f18e176ffeb13a14053b
Jimmy Tsai <JimmyTsai@transglobe.com.tw>
2026-04-14 15:49:42 +0800

    fix(routes): adminUserRoutes absolute paths to relative

--- src/router/adminUserRoutes.ts
+++ src/router/adminUserRoutes.ts
@@ -3,25 +3,25 @@ import type { RouteRecordRaw } from 'vue-router';
 
 const adminUserRoutes: RouteRecordRaw[] = [
   {
-    path: '/home/admin-users',
+    path: 'admin-users',
     name: 'AdminUserList',
     component: () => import('@/views/adminUser/AdminUserList.vue'),
     meta: { requiresAuth: true, layout: 'default' },
   },
   {
-    path: '/home/admin-users/add-owner',
+    path: 'admin-users/add-owner',
     name: 'AdminUserAddOwner',
     component: () => import('@/views/adminUser/AdminUserForm.vue'),
     meta: { requiresAuth: true, layout: 'default' },
   },
   {
-    path: '/home/admin-users/add-editor',
+    path: 'admin-users/add-editor',
     name: 'AdminUserAddEditor',
     component: () => import('@/views/adminUser/AdminUserForm.vue'),
     meta: { requiresAuth: true, layout: 'default' },
   },
   {
-    path: '/home/admin-users/:id',
+    path: 'admin-users/:id',
     name: 'AdminUserDetail',
     component: () => import('@/views/adminUser/AdminUserForm.vue'),
     meta: { requiresAuth: true, layout: 'default' },
